418df627b238c737eff4fcacfe064310be1fb2ad
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-27 17:02:03 +0500

    2211E2 27-05-2023

---
+++ app.js
@@ -0,0 +1,96 @@
+//Bonus Task (Unmarked)
+// let a=+prompt("Enter first number")
+// let b=+prompt("Enter second number")
+// console.log("Before Swapping: a="+a+" b="+b)
+
+// let temp
+
+// temp=a
+// a=b
+// b=temp
+
+// console.log("After Swapping: a="+a+" b="+b)
+
+//Q1
+// let num1=+prompt("Enter first number")
+// let num2=+prompt("Enter second number")
+// let sum=num1+num2
+
+// if(num1===num2){
+//     console.log(sum*3)
+// }
+// else if(num1!==num2){
+//     console.log(sum)
+// }
+// else{
+//     console.log("Error")
+// }
+
+//Q2
+// let num1=+prompt("Enter first number")
+// let num2=+prompt("Enter second number")
+// let product=num1*num2
+// let quotient=num1/num2
+// let sum=num1+num2
+// let diff=num1-num2
+
+// if(num1===8||num2===8){
+//     console.log("Either of the numbers is 8")
+// }
+// else if(sum===8){
+//     console.log("Sum of the numbers is 8")
+// }
+// else if(diff===8){
+//     console.log("Difference of the numbers is 8")
+// }
+// else if(product===8){
+//     console.log("Product of the numbers is 8")
+// }
+// else if(quotient===8){
+//     console.log("Quotient of the numbers is 8")
+// }
+// else{
+//     console.log("Error")
+// }
+
+//Q3
+// let n1=+prompt("Enter number 1")
+// let n2=+prompt("Enter number 2")
+// let n3=+prompt("Enter number 3")
+// if(n1===n2&&n2===n3){
+//     console.log("All the numbers are equal")
+// }
+// else if(n1===n2&&n1!==n3){
+//     console.log("Two Numbers are equal")
+
+// }
+// else if(n2===n3&&n1!==n3){
+//     console.log("Two Numbers are equal")
+// }
+// else if(n1===n3&&n2!==n3){
+//     console.log("Two Numbers are equal")
+// }
+// else if(n1!==n2&&n2!==n3&&n1!==n3){
+//     console.log("No Numbers are equal")
+// }
+// else{
+//     console.log("Error")
+// }
+//Q4
+// let age=+prompt("Please enter your age")
+// let days=age*365
+// console.log(days)
+//Q5
+// let n=+prompt("Enter a number")
+// let diff=n-13
+// if(diff>13){
+//     console.log(n*2)
+// }
+// else if(diff<13){
+//     console.log(n/2)
+// }
+// else{
+//     console.log("Error")
+// }
+
+
